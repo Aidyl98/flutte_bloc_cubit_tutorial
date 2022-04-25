ae68d2ffa8f53420192a3837a04af4475e69f592
Aidyl <jgarciaalbalah@gmail.com>
2022-04-25 19:06:39 -0400

    WeatherCubit: + import

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../data/model/weather.dart';
+
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
