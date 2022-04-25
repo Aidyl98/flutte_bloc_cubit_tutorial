f5ddb956b02722822b252fd8d2d88c87b991f7a5
Aidyl <jgarciaalbalah@gmail.com>
2022-04-25 19:13:26 -0400

    WeatherCubit: + _weatherRepository & getWeather

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutte_bloc_cubit_tutorial/data/weather_repository.dart';
 import 'package:meta/meta.dart';
 
 import '../data/model/weather.dart';
@@ -6,5 +7,17 @@ import '../data/model/weather.dart';
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  final WeatherRepository _weatherRepository;
+
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+
+  Future<void> getWeather(String cityName) async {
+    try {
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    } on NetworkException {
+      emit(WeatherError("Couldn't fetch weather. Is the device online?"));
+    }
+  }
 }
