af60dddacf2284f76627f5637796adf05c210ee6
Aidyl <jgarciaalbalah@gmail.com>
2022-04-25 22:21:35 -0400

    WeatherBloc: + WeatherRepository & mapEventToState

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutte_bloc_cubit_tutorial/data/weather_repository.dart';
 import 'package:meta/meta.dart';
 
 import '../data/model/weather.dart';
@@ -7,9 +8,25 @@ part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial()) {
+  final WeatherRepository _weatherRepository;
+
+  WeatherBloc(this._weatherRepository) : super(WeatherInitial()) {
     on<WeatherEvent>((event, emit) {
       // TODO: implement event handler
     });
+  }
+  @override
+  Stream<WeatherState> mapEventToState(
+    WeatherEvent event,
+  ) async* {
+    if (event is GetWeather) {
+      try {
+        yield WeatherLoading();
+        final weather = await _weatherRepository.fetchWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkException {
+        yield WeatherError("Couldn't fetch weather. Is the device online?");
+      }
+    }
   }
 }
