0cd0cc4087e91da5db92e644e0e5ecb830011dd9
Aidyl <jgarciaalbalah@gmail.com>
2022-04-25 21:44:21 -0400

    WeatherState: + WeatherLoaded & WeatherError

--- lib/bloc/weather_state.dart
+++ lib/bloc/weather_state.dart
@@ -4,3 +4,37 @@ part of 'weather_bloc.dart';
 abstract class WeatherState {}
 
 class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {
+  WeatherLoading();
+}
+
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+  WeatherLoaded(this.weather);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is WeatherLoaded && o.weather == weather;
+  }
+
+  @override
+  int get hashCode => weather.hashCode;
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+  WeatherError(this.message);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is WeatherError && o.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
